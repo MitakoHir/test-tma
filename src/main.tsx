e934774b565a2b0e2a5f35fa3c0a424dcc697c29
Iurii Topychkanov <mitakohir@gmail.com>
2024-06-10 02:34:33 +0300

    add phaser framework to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import WebApp from '@twa-dev/sdk'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
